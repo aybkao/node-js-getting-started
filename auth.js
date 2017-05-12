d50f193826e0f41260a598355ac7492acae76b4d
aybkao <aybkao@gmail.com>
2017-05-12 03:48:20 -0700

    add credentials

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'googleAuth' : {
+    'clientID' : '1065319748679-rpc56alsmg08i64qnm8ml67s19ofakj4.apps.googleusercontent.com',
+    'clientSecret' : '63zopJLWo1y8n60dD2jWiVsj',
+    'callbackURL' : 'http://localhost:5000/auth/google/callback' 
+  }
+}
