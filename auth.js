b05806690aa0d2bd8e9bdc556a7e3506acb3a121
aybkao <aybkao@gmail.com>
2017-05-12 04:08:13 -0700

    change keys

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'googleAuth' : {
-    'clientID' : '1065319748679-rpc56alsmg08i64qnm8ml67s19ofakj4.apps.googleusercontent.com',
-    'clientSecret' : '63zopJLWo1y8n60dD2jWiVsj',
-    'callbackURL' : 'http://localhost:5000/auth/google/callback' 
+    'clientID' : '686751273311-999m4o4eb16bm23j5nvg10vr53grf9lk.apps.googleusercontent.com',
+    'clientSecret' : 'AEjCYrF2vRhmlUI3nEOg0P_L',
+    'callbackURL' : 'http://practicemakesperfect1.herokuapp.com/auth/google/callback' 
   }
 }
