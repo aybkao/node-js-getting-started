aa480d3bb48c0c1464e85d6f5ca37aeb5aab18ca
aybkao <aybkao@gmail.com>
2017-05-12 08:34:18 -0700

    self gitignore

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'googleAuth' : {
-    'clientID' : '686751273311-999m4o4eb16bm23j5nvg10vr53grf9lk.apps.googleusercontent.com',
-    'clientSecret' : 'AEjCYrF2vRhmlUI3nEOg0P_L',
-    'callbackURL' : 'http://practicemakesperfect1.herokuapp.com/auth/google/callback' 
+    'clientID' : '',
+    'clientSecret' : '',
+    'callbackURL' : '' 
   }
 }
